f7c66a01f6cb60a64e544cf0b32e9523888a605b
John Rey Bisnar Calipes <johnreybisnarcalipes@gmail.com>
2025-11-10 14:47:37 +0800

    Update invAnalytic.ts

--- backend/routes/invAnalytic.ts
+++ backend/routes/invAnalytic.ts
@@ -4,10 +4,21 @@ import * as analyticController from "../controllers/invAnalytic.controller.js";
 const router = express.Router();
 
 router.get("/get-analytics-chart", analyticController.getAnalyticsChart);
-router.get("/get-analytics-bottom-section",analyticController.getAnalyticsBottomSection);
-router.get("/get-procurement-metrics",analyticController.getProcurementMetrics);
-router.get("/get-procurement-analytics",analyticController.getProcurementAnalytics);
+router.get(
+  "/get-analytics-bottom-section",
+  analyticController.getAnalyticsBottomSection
+);
+router.get(
+  "/get-procurement-metrics",
+  analyticController.getProcurementMetrics
+);
+router.get(
+  "/get-procurement-analytics",
+  analyticController.getProcurementAnalytics
+);
+
 router.get("/get-department-metrics", analyticController.getDepartmentMetrics);
 router.get("/get-department-charts", analyticController.getDepartmentCharts);
+router.get("/get-analytics-bottom-section", analyticController.getAnalyticsBottomSection);
 
 export default router;
